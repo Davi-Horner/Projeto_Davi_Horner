1c94717c75d933f5a3cc49275c9cb812ca89c859
Davi-Horner <davi.castro@estudante.ufla.br>
2020-06-08 21:25:27 -0300

    Mudança na classe C

--- C.java
+++ C.java
@@ -1,6 +1,6 @@
 public class C(){
-    String C1;
-    int C2;
+    private String C1;
+    private int C2;
 
     public void MC1(){
         System.out.println("MC1");
@@ -8,5 +8,9 @@ public class C(){
     
     public void MC2(){
         System.out.println("MC2");
+    }
+
+    public void MC3(){
+        System.out.println("MC3");
     }
 }
